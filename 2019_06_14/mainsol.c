d7a3f09fc0d58ffafdfe4378176bb6c50b1543e9
Miguel Reyes <reyes.606@hotmail.com>
2019-06-14 12:50:16 -0500

    Adicionando mainsol.c

--- 2019_06_14/mainsol.c
+++ 2019_06_14/mainsol.c
@@ -10,16 +10,23 @@ int main(int argc, char** argv) {
   pid = fork();
 
   if(pid == 0){
-   printf("fibonacci(%d)= ",x);
+   printf("Soy el hijo \n");
+   
+   printf("  calculo fibonacci\n");
    x = fibonacci(x);
-   printf("%d\n",x);
+   printf("  guardo el valor calculado en archivo 'X'");
    guardarEntero("archivo",x);
+   printf("  fibonacci: %d\n",x);
   }
   else{
    int status;
    wait(&status);
+   printf("soy el padre \n");
+   printf("  espero a que termine mi hijo\n");
+   printf("  leo el valor que calculo mi hijo del archivo 'X'\n");
+   printf("  multiplico por 100 e imprimo por pantalla el resultado: ");
    int respuesta = leerEntero("archivo") * 100;
-   printf("la respuesta del padre es: %d\n",respuesta);
+   printf("%d\n",respuesta);
   }
 
   return 0;
